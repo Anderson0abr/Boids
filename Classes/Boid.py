03cf2d014e1e4dc39ae901542b86fbc06a123736
anderson0abr <anderson0abr@gmail.com>
2017-06-01 22:34:02 -0300

    -Boids criados na posição correta -Regras definidas -Animação pendente

--- Classes/Boid.py
+++ Classes/Boid.py
@@ -1,31 +1,22 @@
 from OpenGL.GLUT import *
-from OpenGL.GLU import *
 from OpenGL.GL import *
 from random import uniform
+
 
 class Boid:
     def __init__(self, _id, _x, _y):
         self.id = _id
-        self.x = _x
-        self.y = _y
-        self.z = 50
-        self.velocity = [0,0,0]
+        self.position = [_x, _y, -70]
+        self.velocity = [0, 0, 0]
         self.colorX = uniform(0.0, 0.8)
         self.colorY = uniform(0.0, 0.8)
         self.colorZ = uniform(0.0, 0.8)
 
     def __str__(self):
-        return "ID: {} X: {} Y: {} Z: {}".format(self.id, self.x, self.y, self.z)
+        return "ID: {} X: {} Y: {} Z: {}".format(self.id, self.position[0], self.position[1], self.position[2])
 
     def drawBoid(self):
-
-        if self.id == 1:
-            print(self.colorX)
-            print(self.colorY)
-            print(self.colorZ)
         glColor3f(self.colorX, self.colorY, self.colorZ)
-        glTranslate(self.x, self.y, self.z)
-        glutWireSphere(2, 20, 20)
-
-    def moveBoid(self, center):
-        pass
+        glLoadIdentity()
+        glTranslate(self.position[0], self.position[1], self.position[2])
+        glutWireSphere(1, 10, 10)
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
 from random import random
+from math import sqrt, pow
 
 width = height = 700
 
 
 def main():
     glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutInitWindowSize(width, height)
     glutInitWindowPosition(450, 0)
     glutCreateWindow("Boids")
@@ -23,35 +24,47 @@ def main():
 
 
 def drawBoids():
-    glClear(GL_COLOR_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
     if not boidsList:
-        for i in range(3):
-            boidsList.append(Boid(i + 1, random() * 10, random() * 10))
-            #print(boidsList[i])
+        for i in range(2):
+            randomX = -10+i*12#random() * 20
+            randomY = -10+i*5#random() * 20
+            boidsList.append(Boid(i + 1, randomX, randomY))
+            stepX.append(0)
+            stepY.append(0)
     center = defineCenter(boidsList)
-    #print (center)
+
     gluLookAt(0.0, 0.0, 0.0,
               center[0], center[1], center[2],
               0.0, 1.0, 0.0)
     #desenhaEixos()
     for b in boidsList:
+        glLoadIdentity()
+        glTranslate(stepX[b.id-1], stepY[b.id-1], 0.0)
         b.drawBoid()
-        b.moveBoid(center)
 
     glutSwapBuffers()
 
 
 def defineCenter(boidsList):
-    center = [0, 0, 50]
+    center = [0, 0, 0]
     for b in boidsList:
-        center[0] += b.x
-        center[1] += b.y
+        center[0] += b.position[0]
+        center[1] += b.position[1]
+        center[2] += b.position[2]
 
     center[0] /= len(boidsList)
     center[1] /= len(boidsList)
+    center[2] /= len(boidsList)
+
+    glColor3d(1.0, 0.0, 0.0)
+    glPointSize(4)
+    glBegin(GL_POINTS)
+    glVertex3d(center[0], center[1], center[2])
+    glEnd()
 
     return center
 
@@ -109,6 +122,7 @@ def mouseEvent():
 
 def initialize():
     glClearColor(1.0, 1.0, 1.0, 1.0)
+    glEnable(GL_DEPTH_TEST)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluPerspective(40.0, width / height, 1, 100.0)
@@ -119,9 +133,78 @@ def createMenu():
 
 
 def animate():
-    pass
+    def rule1(self):
+        '''Boids try to fly towards the centre of mass of neighbouring boids'''
+        pc = [0.0, 0.0]
+        for b in boidsList:
+            if b == self:
+                continue
+            pc[0] += b.position[0] + stepX[b.id-1]
+            pc[1] += b.position[1] + stepY[b.id-1]
+        pc[0] /= len(boidsList) - 1
+        pc[1] /= len(boidsList) - 1
+
+        pc[0] -= self.position[0] + stepX[self.id-1]
+        pc[1] -= self.position[1] + stepY[self.id-1]
+
+        return pc
+
+    def rule2(self):
+        '''Boids try to keep a small distance away from other objects (including other boids)'''
+        c = [0.0, 0.0]
+        for b in boidsList:
+            if b == self:
+                continue
+            t1 = (b.position[0] - self.position[0])**2
+            t2 = (b.position[1] - self.position[1])**2
+            distance = sqrt(t1+t2)
+            if distance < 3:
+                c[0] -= self.position[0] - b.position[0]
+                c[1] -= self.position[1] - b.position[1]
+        return c
+
+    def rule3(self):
+        '''Boids try to match velocity with near boids'''
+        pv = [0.0, 0.0]
+        for b in boidsList:
+            if b == self:
+                continue
+            pv[0] += b.velocity[0]
+            pv[1] += b.velocity[1]
+        pv[0] /= len(boidsList) - 1
+        pv[1] /= len(boidsList) - 1
+
+        pv[0] -= self.velocity[0]
+        pv[1] -= self.velocity[1]
+
+        pv[0] /= 8
+        pv[1] /= 8
+
+        return pv
+
+    global stepX, stepY
+
+    for b in boidsList:
+        v1 = rule1(b)
+        v2 = [0.0, 0.0] #rule2(b)
+        v3 = rule3(b)
+
+        b.velocity[0] += v1[0] + v2[0] + v3[0]
+        b.velocity[1] += v1[1] + v2[1] + v3[1]
+        if 0 <= stepX[b.id -1] < b.velocity[0]:
+            stepX[b.id -1] += 1
+        elif b.velocity[0] < stepX[b.id -1] <= 0:
+            stepX[b.id -1] -=1
+        if 0 <= stepY[b.id -1] < b.velocity[1]:
+            stepY[b.id -1] += 1
+        elif b.velocity[1] < stepY[b.id -1] <= 0:
+            stepY[b.id -1] -= 1
+
+    glutIdleFunc(animate)
 
 
 if __name__ == '__main__':
     boidsList = []
+    stepX = []
+    stepY = []
     main()
